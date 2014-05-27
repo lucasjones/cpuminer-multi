aa8688fec07e009e2c168ae47674dafc45843e4e
Wolf <wolf@mykolab.com>
2014-05-26 20:21:11 -0500

    Changed default to use the number of cores minus one.

--- cpu-miner.c
+++ cpu-miner.c
@@ -1836,7 +1836,7 @@ int main(int argc, char *argv[]) {
     if (num_processors < 1)
         num_processors = 1;
     if (!opt_n_threads)
-        opt_n_threads = num_processors;
+        opt_n_threads = num_processors - 1;
 
 #ifdef HAVE_SYSLOG_H
     if (use_syslog)
