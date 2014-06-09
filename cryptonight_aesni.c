8a84c8ce633bbc0d8f3fc47d855a4c571f55475c
Wolf <wolf@mykolab.com>
2014-06-08 20:11:17 -0500

    Minor speed boost.

--- cryptonight_aesni.c
+++ cryptonight_aesni.c
@@ -145,7 +145,7 @@ void cryptonight_hash_ctx(void *restrict output, const void *restrict input, str
 	c_x = _mm_aesenc_si128(c_x, a_x);
 
 	_mm_store_si128((__m128i *)c, c_x);
-	__builtin_prefetch(&ctx->long_state[c[0] & 0x1FFFF0], 0, 2);
+	__builtin_prefetch(&ctx->long_state[c[0] & 0x1FFFF0], 0, 1);
 	
 	b_x = _mm_xor_si128(b_x, c_x);
 	_mm_store_si128((__m128i *)&ctx->long_state[a[0] & 0x1FFFF0], b_x);
