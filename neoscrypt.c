50f955b118bc253787236ab8e3bfdddf8d4d7c5e
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-10-27 15:53:08 +0100

    neoscrypt: use official blake2s

--- neoscrypt.c
+++ neoscrypt.c
@@ -31,6 +31,7 @@
 #include <string.h>
 
 #include "miner.h"
+#include "sha3/blake2s.h"
 
 #if (WINDOWS)
 /* sizeof(unsigned long) = 4 for MinGW64 */
@@ -501,12 +502,31 @@ static void neoscrypt_xor(void *dstp, const void *srcp, uint len) {
     }
 }
 
-
 /* BLAKE2s */
 
 #define BLAKE2S_BLOCK_SIZE    64U
 #define BLAKE2S_OUT_SIZE      32U
 #define BLAKE2S_KEY_SIZE      32U
+
+static const uint blake2s_IV[8] = {
+    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+    0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
+};
+
+#ifdef USE_CUSTOM_BLAKE2S
+
+static const uint8_t blake2s_sigma[10][16] = {
+    {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
+    { 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 } ,
+    { 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 } ,
+    {  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 } ,
+    {  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 } ,
+    {  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 } ,
+    { 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 } ,
+    { 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 } ,
+    {  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 } ,
+    { 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 } ,
+};
 
 /* Parameter block of 32 bytes */
 typedef struct blake2s_param_t {
@@ -531,25 +551,7 @@ typedef struct blake2s_state_t {
     uint  buflen;
 } blake2s_state;
 
-static const uint blake2s_IV[8] = {
-    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
-    0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
-};
-
-static const uint8_t blake2s_sigma[10][16] = {
-    {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
-    { 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 } ,
-    { 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 } ,
-    {  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 } ,
-    {  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 } ,
-    {  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 } ,
-    { 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 } ,
-    { 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 } ,
-    {  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 } ,
-    { 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 } ,
-};
-
-static void blake2s_compress(blake2s_state *S, const uint *buf) {
+static void blake2s_compress(blake2s_state *S, const void *buf) {
     uint i;
     uint m[16];
     uint v[16];
@@ -618,7 +620,7 @@ static void blake2s_update(blake2s_state *S, const uchar *input, uint input_size
             /* Counter increment */
             S->t[0] += BLAKE2S_BLOCK_SIZE;
             /* Compress */
-            blake2s_compress(S, (uint *) S->buf);
+            blake2s_compress(S, (void *) S->buf);
             /* Shift buffer left */
             neoscrypt_copy(S->buf, S->buf + BLAKE2S_BLOCK_SIZE, BLAKE2S_BLOCK_SIZE);
             S->buflen -= BLAKE2S_BLOCK_SIZE;
@@ -633,7 +635,7 @@ static void blake2s_update(blake2s_state *S, const uchar *input, uint input_size
         }
     }
 }
-
+#endif
 static void neoscrypt_blake2s(const void *input, const uint input_size, const void *key, const uchar key_size,
   void *output, const uchar output_size) {
     uchar block[BLAKE2S_BLOCK_SIZE];
@@ -661,14 +663,14 @@ static void neoscrypt_blake2s(const void *input, const uint input_size, const vo
     /* Finish */
     if(S->buflen > BLAKE2S_BLOCK_SIZE) {
         S->t[0] += BLAKE2S_BLOCK_SIZE;
-        blake2s_compress(S, (uint *) S->buf);
+        blake2s_compress(S, (void *) S->buf);
         S->buflen -= BLAKE2S_BLOCK_SIZE;
         neoscrypt_copy(S->buf, S->buf + BLAKE2S_BLOCK_SIZE, S->buflen);
     }
     S->t[0] += S->buflen;
     S->f[0] = ~0U;
     neoscrypt_erase(S->buf + S->buflen, 2 * BLAKE2S_BLOCK_SIZE - S->buflen);
-    blake2s_compress(S, (uint *) S->buf);
+    blake2s_compress(S, (void *) S->buf);
 
     /* Write back */
     neoscrypt_copy(output, S, output_size);
