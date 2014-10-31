4406425a7ca82ce4c2fb1ec963314b9cd2ca72d2
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-10-31 12:35:12 +0100

    mingw64: gcc 4.9 doesnt like every obscure cflags

--- mingw64.sh
+++ mingw64.sh
@@ -7,10 +7,10 @@ SSL_PREFIX=/usr/local/ssl
 extracflags="-O3 -march=native -Wall -D_REENTRANT -funroll-loops -fvariable-expansion-in-unroller -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
 # gcc 4.8+
-extracflags="$extracflags -Ofast -fuse-linker-plugin -ftlo -ftree-loop-if-convert-stores"
+extracflags="$extracflags -Ofast -fuse-linker-plugin -ftree-loop-if-convert-stores" # -flto "
 
 CFLAGS="-DCURL_STATICLIB -DOPENSSL_NO_ASM -DUSE_ASM $extracflags"
 
-./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX CFLAGS="$CFLAGS -Dsize_t=uint32_t"
+./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX CFLAGS="$CFLAGS"
 
 make
