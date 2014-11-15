1a2f549788fef7a6859fc7cc102d784e81f72e76
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-11-15 22:31:09 +0100

    sensors: fix segfault if sysfs not found

--- sensors.c
+++ sensors.c
@@ -15,11 +15,20 @@
 
 #define HWMON_PATH \
  "/sys/class/hwmon/hwmon1/device/temp1_input"
+#define HWMON_ALT \
+ "/sys/class/hwmon/hwmon0/temp1_input"
+
 static float linux_cputemp(int core)
 {
 	float tc = 0.0;
 	FILE *fd = fopen(HWMON_PATH, "r");
 	uint32_t val = 0;
+
+	if (!fd)
+		fd = fopen(HWMON_ALT, "r");
+
+	if (!fd)
+		return tc;
 
 	if (fscanf(fd, "%d", &val))
 		tc = val / 1000.0;
@@ -34,6 +43,9 @@ static uint32_t linux_cpufreq(int core)
 {
 	FILE *fd = fopen(CPUFREQ_PATH, "r");
 	uint32_t freq = 0;
+
+	if (!fd)
+		return freq;
 
 	if (!fscanf(fd, "%d", &freq))
 		return freq;
