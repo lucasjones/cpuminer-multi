5c22a0a68e51c1d8bc7f63eca83c007fd91f00ac
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-02-01 17:56:32 +0100

    windows: add icon and version informations

--- mingw64.sh
+++ mingw64.sh
@@ -13,9 +13,17 @@ extracflags="-O3 -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fva
 CFLAGS="-DCURL_STATICLIB -DOPENSSL_NO_ASM -DUSE_ASM $extracflags"
 # CPPFLAGS=""
 
-./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS"
+# icon
+windres res/icon.rc icon.o
+
+./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX \
+	CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" LDFLAGS="icon.o"
 
 make
 
 strip -p --strip-debug --strip-unneeded cpuminer.exe
+
+if [ -e sign.sh ] ; then
+. sign.sh
+fi
 
