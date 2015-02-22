580d68e22df131eab32ac2983fda274b57a9fd3c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-02-22 04:32:17 +0100

    pluck: optimize

--- algo/pluck.c
+++ algo/pluck.c
@@ -37,8 +37,7 @@
 #define htobe32(x)  ((uint32_t)htonl((uint32_t)(x)))
 #endif
 
-// note, this is 64 bits
-#define ROTL(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
+#define ROTL(a, b) _rotl(a,b)// (((a) << (b)) | ((a) >> (32 - (b))))
 
 #ifdef _MSC_VER
 //#include "scryptjane/scrypt-jane-portable-x86.h"
@@ -62,26 +61,44 @@
 #endif
 
 #ifdef OPT_COMPATIBLE
-static void _VECTOR xor_salsa8(__m128i B[4], const __m128i Bx[4])
+static void _VECTOR xor_salsa8(__m128i B[4], const __m128i Bx[4], int i)
 {
 	__m128i X0, X1, X2, X3;
 
-	X0 = B[0] = _mm_xor_si128(B[0], Bx[0]);
-	X1 = B[1] = _mm_xor_si128(B[1], Bx[1]);
-	X2 = B[2] = _mm_xor_si128(B[2], Bx[2]);
-	X3 = B[3] = _mm_xor_si128(B[3], Bx[3]);
+	if (i <= 128) {
+		// a xor 0 = a
+		X0 = B[0] = Bx[0];
+		X1 = B[1] = Bx[1];
+		X2 = B[2] = Bx[2];
+		X3 = B[3] = Bx[3];
+	} else {
+		X0 = B[0] = _mm_xor_si128(B[0], Bx[0]);
+		X1 = B[1] = _mm_xor_si128(B[1], Bx[1]);
+		X2 = B[2] = _mm_xor_si128(B[2], Bx[2]);
+		X3 = B[3] = _mm_xor_si128(B[3], Bx[3]);
+	}
 
-	for (int i = 0; i < 4; i++) {
+	for (i = 0; i < 4; i++) {
 		/* Operate on columns. */
-		X1.m128i_u32[0] ^= ROTL(X0.m128i_u32[0] + X3.m128i_u32[0], 7);  X2.m128i_u32[1] ^= ROTL(X1.m128i_u32[1] + X0.m128i_u32[1], 7);
-		X3.m128i_u32[2] ^= ROTL(X2.m128i_u32[2] + X1.m128i_u32[2], 7);  X0.m128i_u32[3] ^= ROTL(X3.m128i_u32[3] + X2.m128i_u32[3], 7);
-		X2.m128i_u32[0] ^= ROTL(X1.m128i_u32[0] + X0.m128i_u32[0], 9);  X3.m128i_u32[1] ^= ROTL(X2.m128i_u32[1] + X1.m128i_u32[1], 9);
-		X0.m128i_u32[2] ^= ROTL(X3.m128i_u32[2] + X2.m128i_u32[2], 9);  X1.m128i_u32[3] ^= ROTL(X0.m128i_u32[3] + X3.m128i_u32[3], 9);
+		X1.m128i_u32[0] ^= ROTL(X0.m128i_u32[0] + X3.m128i_u32[0], 7);
+		X2.m128i_u32[1] ^= ROTL(X1.m128i_u32[1] + X0.m128i_u32[1], 7);
+		X3.m128i_u32[2] ^= ROTL(X2.m128i_u32[2] + X1.m128i_u32[2], 7);
+		X0.m128i_u32[3] ^= ROTL(X3.m128i_u32[3] + X2.m128i_u32[3], 7);
 
-		X3.m128i_u32[0] ^= ROTL(X2.m128i_u32[0] + X1.m128i_u32[0], 13);  X0.m128i_u32[1] ^= ROTL(X3.m128i_u32[1] + X2.m128i_u32[1], 13);
-		X1.m128i_u32[2] ^= ROTL(X0.m128i_u32[2] + X3.m128i_u32[2], 13);  X2.m128i_u32[3] ^= ROTL(X1.m128i_u32[3] + X0.m128i_u32[3], 13);
-		X0.m128i_u32[0] ^= ROTL(X3.m128i_u32[0] + X2.m128i_u32[0], 18);  X1.m128i_u32[1] ^= ROTL(X0.m128i_u32[1] + X3.m128i_u32[1], 18);
-		X2.m128i_u32[2] ^= ROTL(X1.m128i_u32[2] + X0.m128i_u32[2], 18);  X3.m128i_u32[3] ^= ROTL(X2.m128i_u32[3] + X1.m128i_u32[3], 18);
+		X2.m128i_u32[0] ^= ROTL(X1.m128i_u32[0] + X0.m128i_u32[0], 9);
+		X3.m128i_u32[1] ^= ROTL(X2.m128i_u32[1] + X1.m128i_u32[1], 9);
+		X0.m128i_u32[2] ^= ROTL(X3.m128i_u32[2] + X2.m128i_u32[2], 9);
+		X1.m128i_u32[3] ^= ROTL(X0.m128i_u32[3] + X3.m128i_u32[3], 9);
+
+		X3.m128i_u32[0] ^= ROTL(X2.m128i_u32[0] + X1.m128i_u32[0], 13);
+		X0.m128i_u32[1] ^= ROTL(X3.m128i_u32[1] + X2.m128i_u32[1], 13);
+		X1.m128i_u32[2] ^= ROTL(X0.m128i_u32[2] + X3.m128i_u32[2], 13);
+		X2.m128i_u32[3] ^= ROTL(X1.m128i_u32[3] + X0.m128i_u32[3], 13);
+
+		X0.m128i_u32[0] ^= ROTL(X3.m128i_u32[0] + X2.m128i_u32[0], 18);
+		X1.m128i_u32[1] ^= ROTL(X0.m128i_u32[1] + X3.m128i_u32[1], 18);
+		X2.m128i_u32[2] ^= ROTL(X1.m128i_u32[2] + X0.m128i_u32[2], 18);
+		X3.m128i_u32[3] ^= ROTL(X2.m128i_u32[3] + X1.m128i_u32[3], 18);
 
 		/* Operate on rows. */
 		X0.m128i_u32[1] ^= ROTL(X0.m128i_u32[0] + X0.m128i_u32[3], 7);  X1.m128i_u32[2] ^= ROTL(X1.m128i_u32[1] + X1.m128i_u32[0], 7);
@@ -103,27 +120,35 @@ static void _VECTOR xor_salsa8(__m128i B[4], const __m128i Bx[4])
 
 #else
 
-static inline void xor_salsa8(uint32_t B[16], const uint32_t Bx[16])
+static inline void xor_salsa8(uint32_t B[16], const uint32_t Bx[16], int i)
 {
 	uint32_t x00,x01,x02,x03,x04,x05,x06,x07,x08,x09,x10,x11,x12,x13,x14,x15;
-	int i;
 
-	x00 = (B[ 0] ^= Bx[ 0]);
-	x01 = (B[ 1] ^= Bx[ 1]);
-	x02 = (B[ 2] ^= Bx[ 2]);
-	x03 = (B[ 3] ^= Bx[ 3]);
-	x04 = (B[ 4] ^= Bx[ 4]);
-	x05 = (B[ 5] ^= Bx[ 5]);
-	x06 = (B[ 6] ^= Bx[ 6]);
-	x07 = (B[ 7] ^= Bx[ 7]);
-	x08 = (B[ 8] ^= Bx[ 8]);
-	x09 = (B[ 9] ^= Bx[ 9]);
-	x10 = (B[10] ^= Bx[10]);
-	x11 = (B[11] ^= Bx[11]);
-	x12 = (B[12] ^= Bx[12]);
-	x13 = (B[13] ^= Bx[13]);
-	x14 = (B[14] ^= Bx[14]);
-	x15 = (B[15] ^= Bx[15]);
+	if (i <= 128) {
+		// a xor 0 = a
+		x00 = B[ 0] = Bx[ 0]; x01 = B[ 1] = Bx[ 1]; x02 = B[ 2] = Bx[ 2]; x03 = B[ 3] = Bx[ 3];
+		x04 = B[ 4] = Bx[ 4]; x05 = B[ 5] = Bx[ 5]; x06 = B[ 6] = Bx[ 6]; x07 = B[ 7] = Bx[ 7];
+		x08 = B[ 8] = Bx[ 8]; x09 = B[ 9] = Bx[ 9]; x10 = B[10] = Bx[10]; x11 = B[11] = Bx[11];
+		x12 = B[12] = Bx[12]; x13 = B[13] = Bx[13]; x14 = B[14] = Bx[14]; x15 = B[15] = Bx[15];
+	} else {
+		x00 = (B[ 0] ^= Bx[ 0]);
+		x01 = (B[ 1] ^= Bx[ 1]);
+		x02 = (B[ 2] ^= Bx[ 2]);
+		x03 = (B[ 3] ^= Bx[ 3]);
+		x04 = (B[ 4] ^= Bx[ 4]);
+		x05 = (B[ 5] ^= Bx[ 5]);
+		x06 = (B[ 6] ^= Bx[ 6]);
+		x07 = (B[ 7] ^= Bx[ 7]);
+		x08 = (B[ 8] ^= Bx[ 8]);
+		x09 = (B[ 9] ^= Bx[ 9]);
+		x10 = (B[10] ^= Bx[10]);
+		x11 = (B[11] ^= Bx[11]);
+		x12 = (B[12] ^= Bx[12]);
+		x13 = (B[13] ^= Bx[13]);
+		x14 = (B[14] ^= Bx[14]);
+		x15 = (B[15] ^= Bx[15]);
+	}
+
 	for (i = 0; i < 8; i += 2) {
 		/* Operate on columns. */
 		x04 ^= ROTL(x00 + x12,  7);  x09 ^= ROTL(x05 + x01,  7);
@@ -171,7 +196,8 @@ static inline void xor_salsa8(uint32_t B[16], const uint32_t Bx[16])
 
 #endif
 
-//computes a single sha256 hash
+// standard sha256 hash
+#if 0
 static void sha256_hash(unsigned char *hash, const unsigned char *data, int len)
 {
 	uint32_t _ALIGN(64) S[16];
@@ -194,53 +220,61 @@ static void sha256_hash(unsigned char *hash, const unsigned char *data, int len)
 	for (i = 0; i < 8; i++)
 		be32enc((uint32_t *)hash + i, S[i]);
 }
+#else
+#include <openssl/sha.h>
+static void sha256_hash(unsigned char *hash, const unsigned char *data, int len)
+{
+	SHA256_CTX ctx;
+	SHA256_Init(&ctx);
+	SHA256_Update(&ctx, data, len);
+	SHA256_Final(hash, &ctx);
+}
+#endif
 
-//hash exactly 64 bytes (ie, sha256 block size)
-static void sha256_hash512(unsigned char *hash, const unsigned char *data)
+// hash exactly 64 bytes (ie, sha256 block size)
+static void sha256_hash512(uint32_t *hash, const uint32_t *data)
 {
 	uint32_t _ALIGN(64) S[16];
 	uint32_t _ALIGN(64) T[16];
+	uchar _ALIGN(64) E[64] = { 0 };
 	int i;
 	sha256_init(S);
 
-	memcpy(T, data, 64);
 	for (i = 0; i < 16; i++)
-		T[i] = be32dec(T + i);
+		T[i] = be32dec(&data[i]);
 	sha256_transform(S, T, 0);
 
-	memset(T, 0, 64);
-	//memcpy(T, data + 64, 0);
-	((unsigned char *)T)[0] = 0x80;
-	for (i = 0; i < 16; i++)
-		T[i] = be32dec(T + i);
-		T[15] = 8 * 64;
-	sha256_transform(S, T, 0);
+	E[3]  = 0x80;
+	E[61] = 0x02; // T[15] = 8 * 64 => 0x200;
+	sha256_transform(S, (uint32_t*) E, 0);
 
 	for (i = 0; i < 8; i++)
-		be32enc((uint32_t *)hash + i, S[i]);
+		be32enc(&hash[i], S[i]);
 }
 
 void pluck_hash(uint32_t *hash, const uint32_t *data, uchar *hashbuffer, const int N)
 {
 	int size = N * 1024;
-	memset(hashbuffer, 0, 64);
-	sha256_hash(hashbuffer, (void*) data, BLOCK_HEADER_SIZE);
+	sha256_hash(hashbuffer, (void*)data, BLOCK_HEADER_SIZE);
+	memset(&hashbuffer[32], 0, 32);
 
 	for(int i = 64; i < size - 32; i += 32)
 	{
+		uint32_t _ALIGN(64) randseed[16];
+		uint32_t _ALIGN(64) randbuffer[16];
+		uint32_t _ALIGN(64) joint[16];
 		//i-4 because we use integers for all references against this, and we don't want to go 3 bytes over the defined area
 		//we could use size here, but then it's probable to use 0 as the value in most cases
 		int randmax = i - 4;
-		uint32_t joint[16], randbuffer[16], randseed[16];
 
 		//setup randbuffer to be an array of random indexes
-		memcpy(randseed, hashbuffer + i - 64, 64);
+		memcpy(randseed, &hashbuffer[i - 64], 64);
 
-		if(i > 128) memcpy(randbuffer, hashbuffer + i - 128, 64);
-		else memset(randbuffer, 0, 64);
+		if(i > 128) memcpy(randbuffer, &hashbuffer[i - 128], 64);
+		//else memset(randbuffer, 0, 64);
 
-		xor_salsa8((void*) randbuffer, (void*) randseed);
-		memcpy(joint, hashbuffer + i - 32, 32);
+		xor_salsa8((void*)randbuffer, (void*)randseed, i);
+		memcpy(joint, &hashbuffer[i - 32], 32);
 
 		//use the last hash value as the seed
 		for (int j = 32; j < 64; j += 4)
@@ -248,19 +282,19 @@ void pluck_hash(uint32_t *hash, const uint32_t *data, uchar *hashbuffer, const i
 			//every other time, change to next random index
 			//randmax - 32 as otherwise we go beyond memory that's already been written to
 			uint32_t rand = randbuffer[(j - 32) >> 2] % (randmax - 32);
-			joint[j >> 2] = *((uint32_t *)(hashbuffer + rand));
+			joint[j >> 2] = *((uint32_t *)&hashbuffer[rand]);
 		}
 
-		sha256_hash512(hashbuffer + i, (unsigned char *) joint);
+		sha256_hash512((uint32_t*) &hashbuffer[i], joint);
 
 		//setup randbuffer to be an array of random indexes
 		//use last hash value and previous hash value(post-mixing)
-		memcpy(randseed, hashbuffer + i - 32, 64);
+		memcpy(randseed, &hashbuffer[i - 32], 64);
 
-		if(i > 128) memcpy(randbuffer, hashbuffer + i - 128, 64);
-		else memset(randbuffer, 0, 64);
+		if(i > 128) memcpy(randbuffer, &hashbuffer[i - 128], 64);
+		//else memset(randbuffer, 0, 64);
 
-		xor_salsa8((void*) randbuffer, (void*) randseed);
+		xor_salsa8((void*)randbuffer, (void*)randseed, i);
 
 		//use the last hash value as the seed
 		for (int j = 0; j < 32; j += 2)
@@ -269,10 +303,6 @@ void pluck_hash(uint32_t *hash, const uint32_t *data, uchar *hashbuffer, const i
 			*((uint32_t *)(hashbuffer + rand)) = *((uint32_t *)(hashbuffer + j + randmax));
 		}
 	}
-
-	//note: off-by-one error is likely here...
-	for(int i = size - 64 - 1; i >= 64; i -= 64)
-		sha256_hash512(hashbuffer + i - 64, hashbuffer + i);
 
 	memcpy(hash, hashbuffer, 32);
 }
@@ -295,7 +325,8 @@ int scanhash_pluck(int thr_id, uint32_t *pdata,
 
 	const uint32_t Htarg = ptarget[7];
 	do {
-		be32enc(&endiandata[19], n);
+		//be32enc(&endiandata[19], n);
+		endiandata[19] = n;
 		pluck_hash(hash, endiandata, scratchbuf, N);
 
 		if (hash[7] <= Htarg && fulltest(hash, ptarget))
