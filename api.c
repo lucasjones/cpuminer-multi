0eccce919d0583bc6a2c9ce2800408ddcd28d456
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2015-03-05 18:08:58 +0100

    api: fix port > 32768

--- api.c
+++ api.c
@@ -514,7 +514,7 @@ static bool check_connect(struct sockaddr_in *cli, char **connectaddr, char *gro
 static void api()
 {
 	const char *addr = opt_api_allow;
-	short int port = opt_api_listen; // 4068
+	unsigned short port = (unsigned short) opt_api_listen; // 4048
 	char buf[MYBUFSIZ];
 	int c, n, bound;
 	char *connectaddr;
