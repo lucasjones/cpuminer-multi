ee9f642334b06288020308e993dd40f69fde9d66
Daniel Thamdrup <dallemon@protonmail.com>
2018-11-02 14:02:28 +0100

    cpuid: fix for intel compiler (#31)
    made an error when using intel compiler. compiles fine now.

--- sysinfos.c
+++ sysinfos.c
@@ -113,9 +113,11 @@ int cpu_fanpercent()
 
 #if !defined(__arm__) && !defined(__aarch64__)
 static inline void cpuid(int functionnumber, int output[4]) {
-#if defined (_MSC_VER) || defined (__INTEL_COMPILER)
-	// Microsoft or Intel compiler, intrin.h included
+#ifdef _MSC_VER
+	// Microsoft compiler, intrin.h included
 	__cpuidex(output, functionnumber, 0);
+#elif defined(__INTEL_COMPILER)
+	__cpuid(output, functionnumber);
 #elif defined(__GNUC__) || defined(__clang__)
 	// use inline assembly, Gnu/AT&T syntax
 	int a, b, c, d;
