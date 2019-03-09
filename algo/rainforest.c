8cbca8d7d7f0893e4755790997d336a3a4631ced
Willy Tarreau <w@1wt.eu>
2019-03-09 14:21:54 +0000

    rainforest: do not emit rf_crc32_table if not needed
    it's only needed on platforms who don't have a CRC32 instruction.

--- algo/rainforest.c
+++ algo/rainforest.c
@@ -329,6 +329,7 @@ const uint8_t rf256_iv[32] = {
 };
 
 // crc32 lookup tables
+#if !defined(__ARM_FEATURE_CRC32)
 const uint32_t rf_crc32_table[256] = {
   /* 0x00 */ 0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,
   /* 0x04 */ 0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
@@ -395,6 +396,7 @@ const uint32_t rf_crc32_table[256] = {
   /* 0xf8 */ 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
   /* 0xfc */ 0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d,
 };
+#endif
 
 // compute the crc32 of 32-bit message _msg_ from previous crc _crc_.
 // build with -mcpu=cortex-a53+crc to enable native CRC instruction on ARM
